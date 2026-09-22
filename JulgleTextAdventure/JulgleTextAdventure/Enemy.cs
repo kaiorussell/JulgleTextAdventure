015963ef9f225969f93ad54e21e0180b28457422
kaiorussell <kaiorussell@gmail.com>
2026-09-22 11:53:14 +0200

    Create Enemy.cs

---
+++ JulgleTextAdventure/JulgleTextAdventure/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JulgleTextAdventure
+{
+    internal class Enemy
+    {
+        public string name;
+        public int maxHP;
+        public int currentHP;
+        public int attack;
+        int attackCounter = 0;
+
+        public Enemy(string Name, int MaxHP, int Attack)
+        {
+            name = Name;
+            maxHP = MaxHP;
+            currentHP = maxHP;
+            attack = Attack;
+        }
+
+        public int DoAttack()
+        {
+            if (attackCounter == 3)
+            {
+                return 
+            }
+            else
+            {
+
+            }
+        }
+
+        int SmallAttack()
+        {
+            return attack;
+        }
+        
+        int BigAttack()
+        {
+            return attack * 2;
+        }
+
+
+
+
+
+    }
+}
